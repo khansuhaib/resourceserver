260d27df863c4d713c7e28c622fd39ba0e056dd4
khansuhaib <khansuhaib@gmail.com>
2021-09-15 14:47:58 -0400

    Update ProjectSecurityConfig.java

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -28,7 +28,7 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().configurationSource(new CorsConfigurationSource() {
 			@Override public CorsConfiguration getCorsConfiguration(HttpServletRequest
 			request) { CorsConfiguration config = new CorsConfiguration();
-			config.setAllowedOrigins(Collections.singletonList("http://angular-web-app-angular.apps-crc.testing:8081"));
+			config.setAllowedOrigins(Collections.singletonList("http://angular-web-app-angular.apps-crc.testing:8080"));
 			config.setAllowedMethods(Collections.singletonList("*"));
 			config.setAllowCredentials(true);
 			config.setAllowedHeaders(Collections.singletonList("*"));
